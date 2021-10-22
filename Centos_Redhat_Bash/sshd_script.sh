329da7de1f3aff2ab07031b3f317382732533502
AidoWedo <40786162+AidoWedo@users.noreply.github.com>
2021-10-22 08:13:39 +0100

    Update sshd_script.sh

--- Centos_Redhat_Bash/sshd_script.sh
+++ Centos_Redhat_Bash/sshd_script.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# Modifies settings in an EC2 AWS Centos 7 image
+# AWS seems to modify sshd settings back to PasswordAuthentication No
+# This fixes that until I can work out why it is being reverted back
+# Keeps PubKey Auth, Password Auth, Disables Root login (this should already be set) and Disables GSSAPI options that cause secondary auth issues
+# Pam will be enabled and is left on
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
 sed -i 's/^\(PubkeyAuthentication \).*/\1yes/' /etc/ssh/sshd_config
 sed -i 's/^\(PasswordAuthentication \).*/\1yes/' /etc/ssh/sshd_config
